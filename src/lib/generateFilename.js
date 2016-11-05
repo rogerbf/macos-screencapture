053fe881229ed9bf39808dcbb946a1695c5efd2e
rogerbf <roger@bergfalk.nu>
2016-11-05 12:55:18 +0100

    formatting

--- src/lib/generateFilename.js
+++ src/lib/generateFilename.js
@@ -1,11 +1,8 @@
 import { randomBytes } from 'crypto'
 
 const base = `screencapture`
-
 const currentDate = () => `-${Date.now()}`
-
 const randomHex = () => `-${randomBytes(2).toString(`hex`)}`
-
 const parseExtension = ext => `.${ext.toString().split(`.`).join(``)}`
 
 export default function generateFilename (ext) {
