0d6de58b44376b4536fdd353a5c39a2106103acf
rogerbf <roger@bergfalk.nu>
2016-11-05 12:54:48 +0100

    format for clarity

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import capture from './lib/screencapture'
 import options from './lib/options'
 
-const screencapture = Object.assign(capture, { options: options })
+const screencapture = Object.assign(
+  capture,
+  { options: options }
+)
 
 export default screencapture
